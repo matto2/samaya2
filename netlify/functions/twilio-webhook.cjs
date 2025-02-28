b4b9f050747540cefec7a0029f04708763449757
Matt O'Leary <matto2@users.noreply.github.com>
2025-02-27 21:13:24 -0800

    progress

--- netlify/functions/twilio-webhook.cjs
+++ netlify/functions/twilio-webhook.cjs
@@ -1,25 +1,30 @@
-import { Handler } from "@netlify/functions";
-import twilio from "twilio";
+const twilio = require("twilio");
 
-const apiKeySid = process.env.TWILIO_API_KEY_SID as string;
-const apiKeySecret = process.env.TWILIO_API_KEY_SECRET as string;
-const twilioNumber = process.env.TWILIO_PHONE_NUMBER as string;
-const myPhoneNumber = process.env.MY_PHONE_NUMBER as string;
-const webhookSecret = process.env.CAL_WEBHOOK_SECRET as string;
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+const myPhoneNumber = process.env.MY_PHONE_NUMBER;
+const webhookSecret = process.env.CAL_WEBHOOK_SECRET;
 
-const twilioClient = twilio(apiKeySid, apiKeySecret);
+const twilioClient = twilio(accountSid, authToken);
 
-export const handler: Handler = async (event) => {
+module.exports.handler = async (event) => {
     try {
         if (!event.body) {
-            return { statusCode: 400, body: JSON.stringify({ error: "No body provided" }) };
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "No body provided" }),
+            };
         }
 
         // Verify Webhook Secret
         const receivedSecret = event.headers["x-cal-secret"];
         if (receivedSecret !== webhookSecret) {
             console.warn("Invalid webhook secret received!");
-            return { statusCode: 401, body: JSON.stringify({ error: "Unauthorized" }) };
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ error: "Unauthorized" }),
+            };
         }
 
         const data = JSON.parse(event.body);
